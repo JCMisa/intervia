59b1c147c09f0643861c91ca2bb9348cd35285a2
MisaJC <johncarlomisa399@gmail.com>
2025-02-04 20:00:09 +0800

    change explore cards gap

--- app/(root)/dashboard/explore/page.tsx
+++ app/(root)/dashboard/explore/page.tsx
@@ -27,7 +27,7 @@ const ExplorePage = async () => {
       </div>
 
       {interviews.data?.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-16">
           {interviews.data?.map((interview) => (
             <div key={interview?.id} className="w-full">
               <InterviewCard interview={interview as InterviewType} />
